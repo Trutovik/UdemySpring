5231fc8d897f95413aacb9fcf90fac2a60a6b8d2
Aliaksandr Sabalevich <casbah@mail.ru>
2020-10-20 18:26:15 +0300

    Chapter 11 Adding XML Support and JSON Support

--- src/main/java/com/sabal/spring5webapp/ui/controller/UserController.java
+++ src/main/java/com/sabal/spring5webapp/ui/controller/UserController.java
@@ -6,6 +6,7 @@ import com.sabal.spring5webapp.ui.model.response.UserRest;
 import com.sabal.spring5webapp.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +16,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path="/{id}")
+    @GetMapping(path="/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserRest getUser(@PathVariable String id) {
         UserRest returnValue = new UserRest();
         UserDto userDto = userService.getUserByUserId(id);
@@ -23,7 +24,10 @@ public class UserController {
         return returnValue;
     }
 
-    @PostMapping
+    @PostMapping(
+            consumes={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
 
         UserRest returnValue = new UserRest();
