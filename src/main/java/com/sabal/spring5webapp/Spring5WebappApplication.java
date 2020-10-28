ac3380c4f416b13d3510dcd8754dc1acb09d8ee3
Aliaksandr Sabalevich <casbah@mail.ru>
2020-10-28 09:12:33 +0300

    Section 16 - Deploying your application

--- src/main/java/com/sabal/spring5webapp/Spring5WebappApplication.java
+++ src/main/java/com/sabal/spring5webapp/Spring5WebappApplication.java
@@ -3,14 +3,21 @@ package com.sabal.spring5webapp;
 import com.sabal.spring5webapp.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class Spring5WebappApplication {
+public class Spring5WebappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5WebappApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Spring5WebappApplication.class);
 	}
 
 	@Bean
