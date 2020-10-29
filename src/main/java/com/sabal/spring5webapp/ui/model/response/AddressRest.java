7585374b48bb37b6eec4c8aab81db99561788d17
Aliaksandr Sabalevich <casbah@mail.ru>
2020-10-29 11:49:58 +0300

    Section 20 - HATEOAS. Spring Boot v. 2.3.0RELEASE

--- src/main/java/com/sabal/spring5webapp/ui/model/response/AddressRest.java
+++ src/main/java/com/sabal/spring5webapp/ui/model/response/AddressRest.java
@@ -1,6 +1,8 @@
 package com.sabal.spring5webapp.ui.model.response;
 
-public class AddressRest {
+import org.springframework.hateoas.RepresentationModel;
+
+public class AddressRest extends RepresentationModel<AddressRest> {
 
     private String addressId;
     private String city;
