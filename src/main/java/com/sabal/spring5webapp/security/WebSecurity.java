0ffccb16434840ee4cad97eb0f7c57931a6f80b4
Aliaksandr Sabalevich <casbah@mail.ru>
2020-11-17 09:33:34 +0300

    Section 26 - H2 In-Memory Database

--- src/main/java/com/sabal/spring5webapp/security/WebSecurity.java
+++ src/main/java/com/sabal/spring5webapp/security/WebSecurity.java
@@ -30,12 +30,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_REQUEST_URL)
                 .permitAll()
+                .antMatchers(SecurityConstants.H2_CONSOLE)
+                .permitAll()
                 .antMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_URL)
                 .permitAll()
                 .anyRequest().authenticated().and().addFilter(getAuthenticationFilter())
                 .addFilter(new AuthorizationFilter(authenticationManager()))
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.headers().frameOptions().disable();
     }
 
     @Override
